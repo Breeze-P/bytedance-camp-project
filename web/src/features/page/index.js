0b77981da247db5260bb782e8531f8b30489c62f
Breeze-P <zhiyuzhao8@gmail.com>
2021-09-26 16:28:47 +0800

    feats: 最后的版本发行版

--- web/src/features/page/index.js
+++ web/src/features/page/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
 import {Stage, Layer, Rect, Circle, Line, Star, Transformer} from 'react-konva';
 import { Layout, Menu, Divider, Input, Row, Col, Button, Popover } from "antd";
